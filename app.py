57e1cf2e8280d08e5bb56eba298ff32e90d00e45
MEHMET ATICI <mehmet@MEHMET-MacBook-Pro-2.local>
2026-02-18 15:50:45 +0100

    Final: Database connection integrated

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask
+import pyodbc
 
 app = Flask(__name__)
 
+# Veritabanı Bağlantı Bilgileri (Buraya kendi şifreni yaz!)
+server = 'library-server-atici.database.windows.net'
+database = 'LibraryDB'
+username = 'adminuser'
+password = 'SeninBelirlediginSifre' # <--- BURAYA AZURE'DA KOYDUĞUN ŞİFREYİ YAZ
+driver= '{ODBC Driver 18 for SQL Server}'
+
 @app.route('/')
 def home():
-    return "Cloud Library Project - Final Assignment is Ready!"
+    try:
+        # Veritabanına bağlanmayı dene
+        conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}')
+        return "<h1>Final Project</h1><p>Status: <b>Connected to Azure SQL Database Successfully!</b></p>"
+    except Exception as e:
+        return f"<h1>Final Project</h1><p>Status: Connected to Web Server but Database error: {str(e)}</p>"
 
 if __name__ == '__main__':
     app.run(debug=True)
